d7c0c3682793ce51ed9f92dd463f2544dfece544
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-08 16:49:30 -0500

    feat: add css

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function name() {
+  navigation.classList.toggle('open')
+}
