9ed32876ed760d6b670bc64956b8093e9dfe2cb9
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-08 19:10:57 -0500

    feat: end of the project

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuToggle = document.querySelector('.menuToggle');
 const navigation = document.querySelector('.navigation');
+const list = document.querySelectorAll('.list');
 
 menuToggle.onclick = function name() {
   navigation.classList.toggle('open')
 }
+
+function activeLink() {
+  list.forEach((item) => item.classList.remove('active'));
+  this.classList.add('active');
+}
+
+list.forEach((item) => item.addEventListener('click', activeLink));
